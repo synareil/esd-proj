fcbd9ce7a557ceb6c326cc098fa8258a80fbd3f3
synareil <weilinang.awl@gmail.com>
2024-03-10 23:07:28 +0800

    tried docker (couldn't work on my end > some database issue)

--- Inventory/app.py
+++ Inventory/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/proj_inventory'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -175,4 +176,4 @@ def update_book(itemID):
         ), 500
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
