de8d5618bf61d13e13128b3428194899effb67ce
Julian Willis <jmwillis.2022@business.smu.edu.sg>
2024-03-11 17:43:57 +0800

    Add containerization for Marketing Content

--- MarketingContent/database.py
+++ MarketingContent/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://myuser:mypassword@localhost/mydatabase"
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://myuser:mypassword@localhost/mydatabase")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
