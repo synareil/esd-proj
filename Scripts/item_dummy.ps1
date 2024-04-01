515027da82d7a795a23639ed0e6a2181fd068711
Julian Willis <jmwillis.2022@business.smu.edu.sg>
2024-04-02 02:29:07 +0800

    Add  more dummy sql

--- Scripts/item_dummy.ps1
+++ Scripts/item_dummy.ps1
@@ -1,2 +1,3 @@
 docker cp item.csv esd-proj-inventory-db-1:/var/lib/mysql-files/item.csv
 Get-Content item_dummy.sql | docker exec -i esd-proj-inventory-db-1 mysql -uroot -ppassword proj_inventory
+Get-Content cart_dummy.sql | docker exec -i esd-proj-cart-db-1 mysql -uroot -ppassword proj_cart 
