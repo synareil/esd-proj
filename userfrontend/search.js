7a379fb2b10342b35af541d52a4dd93b80b3853f
Cheryl Toh Wen Qi <wenqicheryl@gmail.com>
2024-04-02 19:45:34 +0800

    undo my bad merge

--- userfrontend/search.js
+++ userfrontend/search.js
@@ -1,11 +1,11 @@
 //To include this search function in the homepage/index.html
-document.getElementById("searchinput")
-    .addEventListener("keyup", function (event) {
-        event.preventDefault();
-        if (event.keyCode === 13) {
-            document.getElementById("button-addon2").click();
-        }
-    });
+// document.getElementById("searchinput")
+//     .addEventListener("keyup", function (event) {
+//         event.preventDefault();
+//         if (event.keyCode === 13) {
+//             document.getElementById("button-addon2").click();
+//         }
+//     });
 // search script
 function search() {
     keyword = document.getElementById("searchinput").value.toLowerCase();
